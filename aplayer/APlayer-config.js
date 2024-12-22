08ba034cae3da152a8247d9a9bd9e2267ecb4048
AceEdg <178728860+AceEdg@users.noreply.github.com>
2024-12-22 18:04:33 +0800

    Create APlayer-config.js

---
+++ aplayer/APlayer-config.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+	fixed: true,
+	theme: '#1890ff',
+    audio: [{
+        name: 'Papercut',
+        artist: 'Linkin Park',
+        url: 'https://m701.music.126.net/20241222062717/58fadd1ad815ce20a71c3b7bfa7c970d/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/26277599474/a7f9/b75c/be19/3b7680274cbeef543631d3612d7ebf0d.flac?vuutv=d0whp3xjzw8JxAUlrXtVwJL2VuURVEz4qywY/+LbEkRjWGIQDSoO+jpteoN1hWZY59LmcBZy3hR0KC/1LQ2BX8EPUX8dz4yYotF4dN7DWhE=',
+        cover: 'https://p3.music.126.net/q6h3zFrzr6Y3NlQT8rir6w==/109951165373635125.jpg?param=320y320'
+    }]
+});
